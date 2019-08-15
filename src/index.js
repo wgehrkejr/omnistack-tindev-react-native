998299f9e7574d39623380369f6fe9a1c7919604
Werner Gehrke Junior <werner.junior@outlook.com>
2019-08-15 00:38:49 -0300

    Adicionada funcionalidade de match com o socket.io

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
-import React from 'react';
+import React from "react";
+import { YellowBox } from "react-native";
 
-import Routes from './routes';
+YellowBox.ignoreWarnings(["Unrecognized WebSocket"]);
+
+import Routes from "./routes";
 
 export default function App() {
   return <Routes />;
